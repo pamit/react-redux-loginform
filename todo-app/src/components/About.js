3f39e674fad2d22052e0c45ac3dae7e73da62fbb
Payam <payam@icumulus.com.au>
2018-11-01 13:08:16 +1100

    router added

---
+++ todo-app/src/components/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+	constructor(props) {
+		super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="header">
+                    <h1>About</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About;
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './components/About';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route path="/about/" component={About} />
+        </Switch>
+      </Router>    
+      {/* <App /> */}
     </Provider>,
     document.getElementById('root')
 );
